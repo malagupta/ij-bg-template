dcea6d941800699b6efe6a6b653c9411d219a366
GitHub Action <action@github.com>
2020-11-09 05:16:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/malagupta/ijbgtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/malagupta/ijbgtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.malagupta.ijbgtemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/malagupta/ijbgtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/malagupta/ijbgtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.malagupta.ijbgtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.malagupta.ijbgtemplate.MyBundle
 
 class MyApplicationService {
 
